24786c35b57fc4077835e1f7e87cac5285468aa7
prateek959 <prateekkush8081@gmail.com>
2025-02-22 12:59:17 +0530

    add

--- controller/bookController.js
+++ controller/bookController.js
@@ -1,15 +1,25 @@
+import Book from "../models/librarySchema.js";
 import User from "../models/userSchema.js";
 
 
 const checkAvailability = async(req, res)=>{
     try {
-        
+        const id = req.params.id;
+
+        const book = await Book.findById(id);
+
+        if(!book || book.status == "borrowed"){
+            return res.status(400).json({msg:"Book is not available"});
+        }
+
         const data = await User.findOne({email:req.user.email});
 
         if(!data){
             return res.status(400).json({msg:"User not found"});
         }
-        data.BookId.push()
+
+
+        data.BookId.push(id);
         console.log(data.BookId);
         res.send("Done");
 
@@ -19,4 +29,38 @@ const checkAvailability = async(req, res)=>{
     }
 };
 
-export {checkAvailability};
+
+const returnBook = async(req, res)=>{
+    try {
+        const id = req.params.id;
+
+        const book = await Book.findById(id);
+
+        if(!book){
+            return res.status(400).json({msg:"Book is not available"});
+        }
+
+        if(book.status !== "borrowed"){
+            return res.status(400).json({msg:"Currently book is available"});
+        }
+
+        const data = await User.findOne({email:req.user.email});
+
+        if(!data){
+            return res.status(400).json({msg:"User not found"});
+        }
+
+        data.filter((elem)=>{
+            elem !== id
+        });
+
+        res.status(200).json({msg:"Book return successfully"});
+        
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({msg:"Internal Server Error"});
+    }
+};
+
+export {checkAvailability, returnBook};
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { auth, role } from '../middlewear/authMiddlewear.js';
-import { checkAvailability } from '../controller/bookController.js';
+import { checkAvailability, returnBook } from '../controller/bookController.js';
 
 const bookRouter = express.Router();
 
-bookRouter.post('/check',auth,role("member"),checkAvailability);
+bookRouter.post('/borrow',auth,role("member"),checkAvailability);
+bookRouter.post('/return',auth,role("member"),returnBook);
 
 export default bookRouter;
